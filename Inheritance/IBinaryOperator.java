963df348ebc5d17af870a3ecbfebb7da0ea8d062
Jason Smith <js5986@mcla.edu>
2019-09-12 14:09:34 -0400

    small fix

---
+++ Inheritance/IBinaryOperator.java
@@ -0,0 +1,9 @@
+
+public interface IBinaryOperator
+  {
+    public void setOperand1();
+    public void setOperand1();
+    public int getOperand2();
+    public int getOperand2();
+    public int operate();
+  }
