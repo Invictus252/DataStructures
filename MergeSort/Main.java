dee35cb96d0d6704b4bec2bfed96cbfecc2255f1
invictus <invictus@localhost.localdomain>
2019-10-10 23:12:20 -0400

    Round Two

--- MergeSort/Main.java
+++ MergeSort/Main.java
@@ -16,17 +16,16 @@ public class Main
       people.add(new Person("Andy", "Ashton", 44));
       people.add(new Person("Albert", "Denton", 58));
 
-      System.out.println("people ->" + people);
-      Collections.sort(people);
-  		System.out.println("people srtd ->" + people);
-      List ans = mergeSort(people);
-      System.out.println("ans ->" + ans);
+      List<Person> ans = mergeSort(people);
+      System.out.println(ans);
       // -----------------------------------
     }
 
-    public static List mergeSort(List items)
+    public static List<Person> mergeSort(List<Person> items)
     {
-        List sorted = new ArrayList();
+        List<Person> sorted = new ArrayList<Person>();
+        List<Person> left = new ArrayList<Person>();
+        List<Person> right = new ArrayList<Person>();
         if (items.size() == 1) // base case...
         {
             // -----------------------------------
@@ -37,10 +36,13 @@ public class Main
         else // recursive call...
         {
             // -----------------------------------
-            List left = items.subList(0, items.size()/2);
-            List right = items.subList(items.size()/2, items.size());
-            System.out.println("left ->" + left);
-            System.out.println("right ->" + right);
+            for(int i = 0;i <= items.size()/2;i++){
+              left.add(items.get(i));
+              items.remove(i);
+            }
+            for(int j = 0 ;j <= items.size()-1 ;j++){
+              right.add(items.get(j));
+            }
             left = mergeSort(left);
             right = mergeSort(right);
             sorted = merge(left,right);
@@ -50,33 +52,31 @@ public class Main
         return sorted;
     }
     //
-    private static List merge(List partA, List partB)
+    private static List<Person> merge(List<Person> partA, List<Person> partB)
     {
-         List merged = new ArrayList();
-         System.out.println("partA ->" + partA);
-         System.out.println("partB ->" + partB);
+         List<Person> merged = new ArrayList<Person>();
          // -----------------------------------
-         // while(partA.size() != 0 && partB.size() != 0){
-         //   if(partA.get(0).compareTo(partB.get(0)) != 0){
-         //     merged.add(partB.get(0));
-         //     partB.remove(0);
-         //   }
-         //   else{
-         //     merged.add(partA.get(0));
-         //     partA.remove(0);
-         //   }
-         // }
-         // while(partA.size() != 0){
-         //   merged.add(partA.get(0));
-         //   partA.remove(0);
-         // }
-         //
-         // while(partB.size() != 0){
-         //   merged.add(partB.get(0));
-         //   partB.remove(0);
-         // }
+
+         while(partA.size() != 0 && partB.size() != 0){
+           merged.add(partA.get(0));
+           merged.add(partB.get(0));
+           partA.remove(0);
+           partB.remove(0);
+           Collections.sort(merged);
+         }
+         while(partA.size() != 0){
+           merged.add(partA.get(0));
+           partA.remove(0);
+           Collections.sort(merged);
+         }
+
+         while(partB.size() != 0){
+           merged.add(partB.get(0));
+           partB.remove(0);
+           Collections.sort(merged);
+         }
          // -----------------------------------
 
          return merged;
-    }
+        }
 }
